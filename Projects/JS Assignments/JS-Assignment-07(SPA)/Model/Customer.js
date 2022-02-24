1754b8eccfaca16f28a9562b141ebacab0ffbf28
Dilu <dilushanpieris@gmail.com>
2022-02-24 23:25:26 +0530

    started project SPA

---
+++ Projects/JS Assignments/JS-Assignment-07(SPA)/Model/Customer.js
@@ -0,0 +1,31 @@
+function Customer(id,name,address,telephone) {
+    var __custId=id;
+    var __custName=name;
+    var __custAddress=address;
+    var __custTelephone=telephone;
+    
+    this.getCustomerID=function () {
+        return __custId;
+    }
+    this.getCusomerName=function () {
+        return __custName;
+    }
+    this.getCustomerAddress=function () {
+        return __custAddress;
+    }
+    this.getCustomerTelephone=function (){
+        return __custTelephone;
+    }
+    this.setCustomerID=function (id) {
+         __custId=id;
+    }
+    this.setCusomerName=function (name) {
+         __custName=name;
+    }
+    this.setCustomerAddress=function (address) {
+         __custAddress=address;
+    }
+    this.setCustomerTelephone=function (telephone){
+         __custTelephone=telephone;
+    }
+}
